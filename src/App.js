fd76c7668bf371af03d3828e4ffd516d8186c9b0
RuLette <owyong.ru@gmail.com>
2020-09-22 15:49:02 +0100

    mapped api data

--- src/App.js
+++ src/App.js
@@ -2,36 +2,46 @@ import React, { Fragment, useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState({ hits: [] });
   const [vidData, setvidData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsError(false);
       setIsLoading(true);
-      const result = await axios.get("/videotutorials").then((res) => {
-        console.log(res);
-        setData(res.data);
-        setIsLoading(false);
-      });
+
+      try {
+        const result = await axios.get("/videotutorials").then((res) => {
+          console.log(res, "result");
+          setvidData(res.data);
+        });
+      } catch (error) {
+        console.log(error, "error");
+        setIsError(true);
+      }
+      setIsLoading(false);
     };
     fetchData();
-  }, [vidData]);
+  }, []);
 
   return (
     <Fragment>
       <h1>Vid Tutorials</h1>
-      {/* {isLoading ? (
+      {isError && <div>Sorry, there has been an error on this app ...</div>}
+      {isLoading ? (
         <div>Loading ...</div>
       ) : (
         <ul>
-          {data.hits.map((item) => (
-            <li key={item.objectID}>
-              <a href={item.vidData}>{item.title}</a>
+          {/* <p>H{JSON.stringify(vidData)}</p> */}
+
+          {vidData.map((item) => (
+            <li key={item.id}>
+              <a href={item.vidData}>{item.teacherName}</a>
             </li>
           ))}
         </ul>
-      )} */}
+      )}
     </Fragment>
   );
 }
